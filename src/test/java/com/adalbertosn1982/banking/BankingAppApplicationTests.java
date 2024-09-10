4c3c9fd7155f69756062173bbc810bd6542916c6
Adalberto Silveira Napoles <adalbertosn1982@gmail.com>
2024-09-09 23:38:57 -0400

    first commit

---
+++ src/test/java/com/adalbertosn1982/banking/BankingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adalbertosn1982.banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
